44cff0c48ed3bffe4135e856e29ea9acb590d6d1
alice (kapkinEV) <alice@example.com>
2020-03-06 11:32:25 +0300

    code: частное чисел

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-
+// you may type whatever you want
 int main()
 {
     cout << "Enter A and B: ";
@@ -9,5 +9,6 @@ int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
-     << "A * B = " << a * b << '\n';
-}
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
+     }
