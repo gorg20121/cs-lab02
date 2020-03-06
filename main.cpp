9bcb84943aaf89d292ce08029fc2b91eee801bfe
alice (kapkinEV) <alice@example.com>
2020-03-06 11:44:01 +0300

    code: печать максима

--- main.cpp
+++ main.cpp
@@ -11,4 +11,6 @@ cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+     if (a > b) {cout<<" max = " <<a;}
+     else {cout <<" max= " <<b;}
      }
